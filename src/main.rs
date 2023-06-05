3d17db1c8e61c270f0f9bac2402bf4c0c2894065
christian <201805571@post.au.dk>
2023-06-05 16:04:55 +0200

    Made main.rs back to proper setup

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ use wake_implementation::benchmarks::benchmarking::{benchmark_sizes, benchmark_p
 
 #[cfg(not(tarpaulin_include))]
 fn main() -> Result<(), Box<dyn Error>> {
-    let protocol_sample_size = 2;
-    let sign_and_session_auth_sample_size = 10;
-    let party_amounts = vec![2, 3];
+    let protocol_sample_size = 15;
+    let sign_and_session_auth_sample_size = 100;
+    let party_amounts = vec![2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 25];
 
     let _ = benchmark_sizes();
     let _ = benchmark_protocol(protocol_sample_size, &party_amounts)?;
